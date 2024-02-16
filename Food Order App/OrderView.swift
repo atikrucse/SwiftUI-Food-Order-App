0a0378bb5a37964133d902e5dddbac4c42737e87
Mynul Atik <53344044+atikrucse@users.noreply.github.com>
2024-02-17 00:40:35 +0600

    Add Tab Bar

---
+++ Food Order App/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  Food Order App
+//
+//  Created by Atik  on 17/2/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
