18860aa1dd81720e89e0bcb850b8c30cff8649c6
Mynul Atik <53344044+atikrucse@users.noreply.github.com>
2024-02-17 00:12:49 +0600

    Initial

---
+++ Food Order App/Food_Order_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Order_AppApp.swift
+//  Food Order App
+//
+//  Created by Atik  on 17/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Food_Order_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiftUI-Food-Order-App
