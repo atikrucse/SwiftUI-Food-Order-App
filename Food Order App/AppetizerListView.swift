8ac2ce55ad5c4c89d951cfe6175c6eb6cefdfdc7
Mynul Atik <53344044+atikrucse@users.noreply.github.com>
2024-02-17 00:49:56 +0600

    Setup Navigation View

---
+++ Food Order App/AppetizerListView.swift
@@ -0,0 +1,21 @@
+//
+//  AppetizerListView.swift
+//  Food Order App
+//
+//  Created by Atik  on 17/2/24.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Appetizer List View")
+                .navigationTitle("🍟 Appetizers")
+        }
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
---
+++ Food Order App/AppetizerTabView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Food Order App
+//
+//  Created by Atik  on 17/2/24.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
+        }
+        .accentColor(Color("brandPrimary"))
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
